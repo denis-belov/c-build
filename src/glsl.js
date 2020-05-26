0aa6225a3b5f8d0a489da7a9c1307a936e84699f
Denis Belov <dsbelov63@gmail.com>
2020-05-26 13:18:26 +0300

    up

--- src/glsl.js
+++ src/glsl.js
@@ -24,7 +24,7 @@ const chalk = require('chalk');
 const { VULKAN } = require('./consts');
 const log = console.log.bind(console);
 
-const execSync = (cmd) => new Promise((resolve) => exec(cmd, { maxBuffer: 0xFFFFFFFF }, (err, stdout, stderr) => resolve([ err, stdout /* , stderr */ ])));
+const execSync = (cmd) => new Promise((resolve) => exec(cmd, { maxBuffer: 0xFFFFFFFF }, (err, stdout, stderr) => resolve([ err, stdout, stderr ])));
 
 const $ = (bin, arg = '') => {
   const name = win32.basename(bin);
@@ -37,40 +37,71 @@ const run = async (src_file, dst_file) => {
   if (fs.lstatSync(src_file).isFile()) {
     let file = fs.readFileSync(src_file, 'utf8');
 
-    while (true) {
-      const match = file.match(/[A-Za-z0-9_-]+.(comp|frag|geom|tesc|tese|vert)\(\{[^]*?\}\)/);
+    const matches = file.match(/[A-Za-z0-9_-]+.(opengl|vulkan).(comp|frag|geom|tesc|tese|vert)\(\{[^]*?\}\)/g);
 
-      if (!match) {
-        break;
-      }
+    let result = '';
 
-      const glsl_code_wrapper = match[0];
-      const glsl_code = glsl_code_wrapper.match(/\(\{[^]*?\}\)/)[0].slice(2, -2);
-      const glsl_filename = glsl_code_wrapper.match(/[A-Za-z0-9_-]+.(comp|frag|geom|tesc|tese|vert)/)[0];
-      const glsl_path = join(glsl_filename);
-      let tab = 0;
+    await new Promise((resolve) => {
+      // log(matches);
+      matches.forEach(async (match, index, arr) => {
+        const glsl_code_wrapper = match;
+        const glsl_code = glsl_code_wrapper.match(/\(\{[^]*?\}\)/)[0].slice(2, -2);
+        const glsl_filename = glsl_code_wrapper.match(/[A-Za-z0-9_-]+.(opengl|vulkan).(comp|frag|geom|tesc|tese|vert)/)[0];
+        const glsl_path = join(__dirname, '../.', glsl_filename);
+        let tab = 0;
 
-      log(glsl_filename);
+        log(glsl_filename);
 
-      fs.writeFileSync(glsl_path, glsl_code.split('\r').slice(1, -1).map((elm, row_index) => {
-        tab = tab || (elm.match(/( )+/) || [ '' ])[0].length;
-        const row = elm.replace(/\n/g, '');
-        const row_tab = (row.match(/( )+/) || [ '' ])[0].length;
-        const new_row = row.slice(row_tab < tab ? row_tab : tab);
-        log(chalk.yellow(`${ row_index }:`), chalk.green(new_row));
-        return new_row;
-      }).join('\n'));
+        fs.writeFileSync(glsl_path, glsl_code.split('\r').slice(1, -1).map((elm, row_index) => {
+          tab = tab || (elm.match(/( )+/) || [ '' ])[0].length;
+          const row = elm.replace(/\n/g, '');
+          const row_tab = (row.match(/( )+/) || [ '' ])[0].length;
+          const new_row = row.slice(row_tab < tab ? row_tab : tab);
+          log(chalk.yellow(`${ row_index + 1 }:`), chalk.green(new_row));
+          return new_row;
+        }).join('\n'));
 
-      await execSync($(join(VULKAN, 'Bin/glslangValidator.exe'), glsl_path))
-      fs.unlinkSync(glsl_path);
+        if (glsl_filename.match(/.opengl./)) {
+          const [ err, stdout, stderr ] = await execSync($(join(VULKAN, 'Bin/glslangValidator.exe'), glsl_path));
 
-      file = `${ file.slice(0, match.index) }${ glsl_code.trim().split('\r\n').map((elm) => `"${ elm }\\n"`).join('\n') }${ file.slice(match.index + glsl_code_wrapper.length) }`;
-    }
+          log(err);
+          log(stdout);
+          log(stderr);
 
-    fs.writeFileSync(dst_file, file);
+          fs.unlinkSync(glsl_path);
+
+          result += `const char* ${ glsl_filename.split('.')[0] }_${ glsl_filename.split('.')[1] } = ${ glsl_code.trim().split('\r\n').map((elm) => `"${ elm }\\n"`).join('\n') };\n\n`;
+          // log(result);
+        } else if (glsl_filename.match(/.vulkan./)) {
+          const spirv_path = join(__dirname, '../.', `${ glsl_filename.split('.').pop() }.spv`);
+
+          const [ err, stdout, stderr ] = await execSync($(join(VULKAN, 'Bin/glslangValidator.exe'), `-V -H -Os ${ glsl_path } -o ${ spirv_path } --spirv-dis`));
+
+          log(err);
+          log(stdout);
+          log(stderr);
+
+          fs.unlinkSync(glsl_path);
+
+          const test = glsl_code.trim().split('');
+
+          // file = `${ file.slice(0, match.index) }{${ new Uint32Array(new Uint8Array(test.map((elm, i) => elm.charCodeAt(0)).concat(new Array(4 - (test.length % 4)).fill(0))).buffer) }}${ file.slice(match.index + glsl_code_wrapper.length) }`;
+          result += `const uint32_t ${ glsl_filename.split('.')[0] }_${ glsl_filename.split('.')[1] }[] = { ${ new Uint32Array(new Uint8Array(fs.readFileSync(spirv_path)).buffer) } };\n\n`;
+          // log(result);
+
+          fs.unlinkSync(spirv_path);
+        }
+
+        (index === (arr.length - 1)) && resolve();
+      });
+    });
+
+    fs.writeFileSync(dst_file, result);
   }
 };
 
 const [ src_file, dst_file ] = process.argv.slice(2);
 
-run(src_file, dst_file);
+(async () => {
+  await run(src_file, dst_file);
+})();
