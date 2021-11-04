c56ea8f38b5fe9c6b67f64a9e97e9e824ed2aa5f
denis-belov <dsbelov63@gmail.com>
2021-11-04 14:22:49 +0400

    up

--- src/spv2h.js
+++ src/spv2h.js
@@ -2,4 +2,8 @@ const fs = require('fs');
 
 const [ src_file, dst_file, name ] = process.argv.slice(2);
 
-fs.writeFileSync(dst_file, `const uint32_t ${ name }[] = { ${ new Uint32Array(new Uint8Array(fs.readFileSync(src_file)).buffer) } };\n`);
+// fs.writeFileSync(dst_file, `const uint32_t ${ name }[] = { ${ new Uint32Array(new Uint8Array().buffer).join(', ') } };\n`);
+
+// fs.writeFileSync(dst_file, `const uint32_t ${ name }[] = { ${ new Uint32Array(new Uint8Array(fs.readFileSync(src_file)).buffer) } };\n`);
+
+fs.writeFileSync(dst_file, `${ new Uint32Array(new Uint8Array(fs.readFileSync(src_file)).buffer).join(', ') }`);
